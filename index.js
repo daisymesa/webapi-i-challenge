6c7d33baef1fdcbfda67fd58d7ad81c0c83278ed
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-10 16:47:54 -0400

    Updated dependencies, created server on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // implement your API here
+/*
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).send('Hello again Web19!')
+})
+
+server.listen(4000, () => console.log('My second express server is running on port 4000!'));
+*/
+
+const express = require('express'); // import the express package
+
+const server = express(); // creates the server
+
+// handle requests to the root of the api, the / route
+server.get('/', (req, res) => {
+  res.send('Hello from Express');
+});
+
+// watch for connections on port 5000
+server.listen(5000, () =>
+  console.log('Server running on http://localhost:5000')
+);
